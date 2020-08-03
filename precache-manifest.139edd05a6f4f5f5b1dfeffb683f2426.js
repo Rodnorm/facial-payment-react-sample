ca5a8dfdee572ff76d10fc77829a9c2d8fb5150c
Rodrigo Normando <r.normando@hotmail.com>
2020-08-03 14:56:57 +0200

    Updates

--- precache-manifest.139edd05a6f4f5f5b1dfeffb683f2426.js
+++ precache-manifest.139edd05a6f4f5f5b1dfeffb683f2426.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "679200379c81976e2999bdc65ba49100",
+    "revision": "1f5cca303abe00b60de78080569b7537",
     "url": "/facial-payment-react-sample/index.html"
   },
   {
-    "revision": "0732ac47d6e7039ebda9",
+    "revision": "a08e8fcba489ba426bb3",
     "url": "/facial-payment-react-sample/static/css/main.604c5d2f.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/facial-payment-react-sample/static/js/2.32b53cd7.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0732ac47d6e7039ebda9",
-    "url": "/facial-payment-react-sample/static/js/main.ee7992e4.chunk.js"
+    "revision": "a08e8fcba489ba426bb3",
+    "url": "/facial-payment-react-sample/static/js/main.49c15682.chunk.js"
   },
   {
     "revision": "cef3e10583ed64868416",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/facial-payment-react-sample/precache-manifest.f795d2756dcfbbf80acac7eccb3bca0a.js"
+  "/facial-payment-react-sample/precache-manifest.139edd05a6f4f5f5b1dfeffb683f2426.js"
 );
 
 self.addEventListener('message', (event) => {
