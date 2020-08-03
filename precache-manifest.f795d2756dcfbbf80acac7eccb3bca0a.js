02ea1cc71d85892e5b560e51b5fcb0fa753e115d
Rodrigo Normando <r.normando@hotmail.com>
2020-08-03 14:42:28 +0200

    Updates

---
+++ precache-manifest.f795d2756dcfbbf80acac7eccb3bca0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "679200379c81976e2999bdc65ba49100",
+    "url": "/facial-payment-react-sample/index.html"
+  },
+  {
+    "revision": "0732ac47d6e7039ebda9",
+    "url": "/facial-payment-react-sample/static/css/main.604c5d2f.chunk.css"
+  },
+  {
+    "revision": "66d1d054c31c26d4a7ac",
+    "url": "/facial-payment-react-sample/static/js/2.32b53cd7.chunk.js"
+  },
+  {
+    "revision": "1d223435c35525049989de575de07279",
+    "url": "/facial-payment-react-sample/static/js/2.32b53cd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0732ac47d6e7039ebda9",
+    "url": "/facial-payment-react-sample/static/js/main.ee7992e4.chunk.js"
+  },
+  {
+    "revision": "cef3e10583ed64868416",
+    "url": "/facial-payment-react-sample/static/js/runtime-main.7f60805b.js"
+  }
+]);
